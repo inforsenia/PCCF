e3431fa9c215a98360dc377bf64d39b005e8c906
Angel Berlanas Vicente <aberlanas@disroot.org>
2025-10-19 19:58:40 +0200

    fix: Algunos cambios para sobrevivir a las formulas

--- tools/excel-to-plan-formativo.py
+++ tools/excel-to-plan-formativo.py
@@ -60,8 +60,14 @@ except KeyError as ke:
 print(" * [ PCCF ] : Generando plan de Formacion en Empresa ")
 for fila in range(fila_inicio, ws.max_row + 1):
         celda_actual = ws[f"{columna_a_buscar}{fila}"]
-        if hoja == "ISO": print(str(celda_actual.value))
-        if celda_actual.value is not None and celda_actual.data_type != 'f':
+        # Comprobamos que el valor de la celda no es la suma simple
+        # esto no es lo mejor, pero es que no se me ocurre otra cosa
+        procesar_forzado=False
+        if celda_actual.data_type == 'f' and "SUM" not in str(celda_actual.value):
+            print("  [ Plan Formativo ] : Celda con formula expresada voluntariamente ")
+            procesar_forzado=True
+            
+        if celda_actual.value is not None and celda_actual.data_type != 'f' or procesar_forzado:
             # Obtener la columna anterior (ej: 'B' si columna_a_buscar es 'C')
             columna_anterior = chr(ord(columna_a_buscar) - 7)
             celda_anterior = ws[f"{columna_anterior}{fila}"]
@@ -73,6 +79,8 @@ for fila in range(fila_inicio, ws.max_row + 1):
                     celda_fusionada = ws[merged_range.start_cell.coordinate]
                     strings.add(str(celda_fusionada.value))
             strings.add(str(celda_anterior.value))
+        else:
+            pass
 
 ras_ordenados=sorted(strings)
 generar_tabla_markdown(ras_ordenados,hoja_orig)
