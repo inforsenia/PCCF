102cbdde41eff36367fcaa5f06ec38d4b8cbe313
Angel Berlanas Vicente <aberlanas@disroot.org>
2025-10-19 19:59:04 +0200

    Utilidades con todos los modulos

--- tools/pccf_utils.py
+++ tools/pccf_utils.py
@@ -6,13 +6,41 @@ def get_hoja_label(hoja):
     if hoja.startswith("Sostenibilidad"): hoja="Sostenibilidad"
     if hoja.startswith("Itinerario personal para la empleabilidad II"): hoja="IPE2"
     if hoja.startswith("Itinerario personal para la empleabilidad I"): hoja="IPE1"
-    if hoja.startswith("Digitalización aplicada a los sectores productivos (GS)"): hoja="Digitalización"
+    if hoja.startswith("Digitalización"): hoja="Digitalización"
     if hoja.startswith("Inglés Profesional"): hoja ="Inglés"
 
+    if hoja.startswith("Montaje y "): hoja="MME"
+    if hoja.startswith("Sistemas operativos monopuesto"): hoja="SOM"
+    if hoja.startswith("Aplicaciones ofimáticas"): hoja="AOF"
+    if hoja.startswith("Sistemas operativos en red"): hoja="SOX"
+    if hoja.startswith("Seguridad informática"): hoja="SIN"
+    if hoja.startswith("Servicios en red"): hoja="SRE"
+    if hoja.startswith("Aplicaciones web"): hoja="AW"
+    if hoja.startswith("Redes Locales"): hoja="Redes"
+
+    # DAM DAW
+    
+    if hoja.startswith("Entornos de"): hoja = "ED"
+    if hoja.startswith("Sistemas Informáticos"): hoja="SI"
+    if hoja.startswith("Bases de "): hoja="BBDD"
+    if hoja.startswith("Programación"): hoja="PROG"
 
     # DAW
     if hoja.startswith("Desarrollo web en entorno servi") : hoja="DWES"
     if hoja.startswith("Desarrollo Web en Entorno Clien") : hoja="DWEC"
+    if hoja.startswith("Despliegue de") : hoja="DAW"
+    if hoja.startswith("Diseño de interfaces web") : hoja="DIW"
+
+    # DAM
+    if hoja.startswith("Desarrollo de Inter") : hoja="DI"
+    if hoja.startswith("Acceso a ") : hoja="AD"
+    if hoja.startswith("Programación multimedia y dispo"): hoja="PMDM"
+    if hoja.startswith("Programación de servicios y pro"): hoja="PSP"
+    if hoja.startswith("Sistemas de gestión empresarial"): hoja="SGE"
+
+    
+    
+    
 
     # ASIR
     if hoja.startswith("Implantación de Siste"): hoja = "ISO"
